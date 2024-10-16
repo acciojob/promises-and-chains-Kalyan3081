4d8ac5c9dc222509af4330f4bf9e2df6ed89c661
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-10-16 22:14:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const vote=new Promise((resolve, reject)=>{
+	if (age>18) {
+		resolve: you can vote
+	}else{
+		reject: you cant vote
+	}
+})
+
+.then((message)=>{
+	console.log(`Welcome, ${name}. You can vote`)
+})
+.catch((error)=>{
+	console.log(`Oh sorry ${name}. You aren't old enough`)
+})
+
+
+
+
+
+
